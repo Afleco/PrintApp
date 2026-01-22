abb24cdbc6ce83a090c9d616358000d55aab0a59
Alejandro <aflecodev@gmail.com>
2026-01-22 09:39:09 +0000

    Splash Screen added - Login v0.2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,41 @@
 import { Stack } from "expo-router";
+
 import AuthProvider from "../providers/AuthProvider";
 
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+
+import SplashScreenComponent from './components/SplashScreen';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [appReady, setAppReady] = useState(false);
+  const [animationFinished, setAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 100)); 
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!appReady || !animationFinished) {
+    return (
+      <SplashScreenComponent 
+        onFinish={() => setAnimationFinished(true)} 
+      />
+    );
+  }
+  
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }}>
