ee72a07a09bc378a1da5ec08cf5d94e7631efa07
Alejandro <aflecodev@gmail.com>
2026-01-27 15:51:55 +0000

    Minimal code review and changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -40,8 +40,8 @@ export default function RootLayout() {
       <AlertProvider> 
         <AuthProvider>
           <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="signin" options={{ headerShown: false }} />
-            <Stack.Screen name="signup" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)/signin" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
             <Stack.Screen name="index" options={{ headerShown: false }} />
           </Stack>
         </AuthProvider>
